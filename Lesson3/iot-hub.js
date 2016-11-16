3ca239705026b2fb9b408e536e714f67f09788e0
Yuwei Zhou <yuwzho@microsoft.com>
2016-11-16 15:51:38 +0800

    Merge run and read in Lesson3

--- Lesson3/iot-hub.js
+++ Lesson3/iot-hub.js
@@ -5,64 +5,38 @@
 
 var EventHubClient = require('azure-event-hubs').Client;
 var blePrinter = require('./ble-message-printer.js');
-var moment = require('moment');
-var iotHubClient;
 
-/**
- * Read device-to-cloud messages from IoT Hub.
- * @param {object}  config - config object
- */
-var readIoTHub = function(config, timeout) {
-  // Receive device-to-cloud messages
+// Close connection to IoT Hub.
+IoTHubReaderClient.prototype.stopReadMessage = function() {
+  this.iotHubClient.close();
+}
+
+// Read device-to-cloud messages from IoT Hub.
+IoTHubReaderClient.prototype.startReadMessage = function(consumerGroupName) {
   var printError = function(err) {
-    console.log(err.message);
+    console.error(err.message || err);
   };
 
-  // Only receive messages sent to IoT Hub after this point in time
-  var startTime = Date.now() - 10000;
-
-  // Use 10 seconds for iotHubClient to bind partition
-  var timer = Date.now() + 10000;
-
-  timeout = timeout || 5000;
-  var interval = setInterval(() => {
-    if(Date.now() - timer >= timeout) {
-      console.log('[' + moment().format('YYYY:MM:DD[T]h:mm:ss') + '] No new trace in the past ' + timeout / 1000 + ' second(s)');
-      console.log('[' + moment().format('YYYY:MM:DD[T]h:mm:ss') + '] Stop reading from your IoT hub');
-      cleanup();
-      clearInterval(interval);
-    }
-  }, 1000);
-
-  iotHubClient = EventHubClient.fromConnectionString(config.iot_hub_connection_string);
-  iotHubClient.open()
-    .then(iotHubClient.getPartitionIds.bind(iotHubClient))
+  this.iotHubClient.open()
+    .then(this.iotHubClient.getPartitionIds.bind(this.iotHubClient))
     .then(function(partitionIds) {
       return partitionIds.map(function(partitionId) {
-        return iotHubClient.createReceiver(config.iot_hub_consumer_group_name, partitionId, {
-          'startAfterTime': startTime
+        return this.iotHubClient.createReceiver(consumerGroupName, partitionId, {
+          'startAfterTime': Date.now() - 10000
         })
         .then(function(receiver) {
-          receiver.on('errorReceived', (error) => {
-            printError(error);
-            timer = Date.now();
-          });
+          receiver.on('errorReceived', printError);
           receiver.on('message', (message) => {
             blePrinter('IoT hub', message.body);
-            timer = Date.now();
           });
         });
-      });
-    })
+      }.bind(this));
+    }.bind(this))
     .catch(printError);
 }
 
-/**
- * Close connection to IoT Hub.
- */
-var cleanup = function() {
-  iotHubClient.close();
+function IoTHubReaderClient(connectionString) {
+  this.iotHubClient = EventHubClient.fromConnectionString(connectionString);
 }
 
-module.exports.readIoTHub = readIoTHub;
-module.exports.cleanup = cleanup;
+module.exports = IoTHubReaderClient;
